657be8af5fc240ca3637746eedeb842af1ae98fc
Haji <hajimursalzade@gmail.com>
2024-03-24 20:08:58 +0400

    10 fixed

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 --  lists all records of the table
 --  ordered by score (top first)
-SELECT score, name FROM second_table ORDER BY score, name DESC;
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC; 
